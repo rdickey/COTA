2777e4b6fbc3e72d2a81ec7bb83afeae4edf5729
Ross Dickey <rdickey@copperegg.com>
2014-04-13 00:04:52 -0500

    more equations

--- orbital_calculations/spaceapps.rb
+++ orbital_calculations/spaceapps.rb
@@ -1,46 +1,113 @@
 #!/usr/bin/env ruby
 
-@ionEngine = {
-  :thrust_N => 1.0,
-  :power_kW => 50.0,
-  :eff_pct => 70.0,
-  :Isp_s => 5000.0,
-  :Ve_km_s => 50.0
+@engine_type = {
+  :ion => {
+    :thrust_N => 0.092,
+    :power_kW => 2.3,
+    :eff_pct => 70.0,
+    :isp_s => 3100.0,
+    :Ve_km_s => 31.0,
+    :mass_kg => 30,  # estimated based on a paper
+    :propellant_used_kg_s => 0.00000227777796  # estimated based on http://www.grc.nasa.gov/WWW/ion/past/90s/nstar.htm
+  },
+  :vasimr => {
+    :thrust_N => 5.0,
+    :power_kW => 200.0,
+    :eff_pct => 50.0,
+    :isp_s => 5000.0,
+    :Ve_km_s => 50.0,
+    :mass_kg => 260,  # estimated based on a paper
+    :propellant_used_kg_s => 0.00805152979066  # estimated based on 23d trip with 8t of fuel
+  }
 }
 
-@power_per_kg_solar_at_1AU = FIXME
+# http://en.wikipedia.org/wiki/Solar_panels_on_spacecraft
+# "Solar arrays producing 300 W/kg and 300 W/m^2 from the sun's 1366 W/m^2 power near the Earth are available."
+@power_per_kg_solar_at_1AU_kW = 0.3
+@power_per_m2_solar_at_1AU_kW_m2 = 0.3
 
-def massFull(mass_empty, mass_propellant)
-  return mass_empty + mass_propellant
+# spitballing here
+@mass_spacecraft_minus_engines_and_array_kg = 5000.0
+@power_required_minus_engines_and_fuel_creation_kW = 10.0
+
+
+# start figuring stuff out
+
+def massSpacecraft(engine_specs, engine_count)
+  return massSolarArray(engine_specs, engine_count) + massEngines(engine_specs, engine_count) + @mass_spacecraft_minus_engines_and_array_kg
 end
 
-def massEmpty(mass_solar_array, mass_engines, mass_fuel_tank, mass_other)
-  return mass_solar_array + mass_engines + mass_fuel_tank + mass_other
+def massSolarArray(engine_specs, engine_count)
+  return powerRequiredTotal(engine_specs, engine_count) / @power_per_kg_solar_at_1AU_kW
 end
 
-def massSolarArray(total_power_required, power_per_kg)
-  return power_per_kg / total_power_required
+def areaSolarArray(engine_specs, engine_count)
+  return powerRequiredTotal(engine_specs, engine_count) / @power_per_m2_solar_at_1AU_kW_m2
 end
 
-# deltaV_km_s = Isp * g * ln ( (mass_asteroid_start_kg)) / mass_asteroid_end_kg)
-# (mass_asteroid_start_kg) / mass_asteroid_end_kg = e ^ (deltaV_km_s / (Isp * g)
-# mass_asteroid_end_kg = mass_asteroid_start_kg / (e ^ (deltaV_km_s / (Isp * g)))
-def massAsteroidEnd(deltaV_km_s, Isp, mass_asteroid_start_kg)
-  return mass_asteroid_start_kg / (Math::E ** (deltaV_km_s / (Isp * 9.8)))
+def massEngines(engine_specs, engine_count)
+  return engine_specs[:mass_kg] * engine_count
 end
 
-def timeOfJourney(FIXME)
+# Need enough power to continually run engines at required power_kW,
+# PLUS turning ice into H2 and O2
+# PLUS all the extra bullshit
+def powerRequiredTotal(engine_specs, engine_count)
+  return (engine_specs[:power_kW] * engine_count) + powerFuelCreation(engine_specs, engine_count) + @power_required_minus_engines_and_fuel_creation_kW
+end
+
+# http://www.space.com/51-asteroids-formation-discovery-and-exploration.html
+#   "The average temperature of the surface of a typical asteroid is minus 100 degrees F (minus 73 degrees C)"
+#   Energy to raise mass of ice to 0C
+#   Energy = mass_kg * 1000 (g/kg) * 73K * 2J/g/K = mass_kg * 146000 J/kg
+#   Power to raise mass of ice to 0C
+#   Power = mass_kg * 146000 K/kg/s
+#   Energy to change a mass of ice into water:
+#   Energy = mass * Lf = mass_kg * 334000 J/kg
+#   Power to raise a mass of ice into water per second
+#   Power = mass_kg * 333550 J/kg/s
+# Assume 5 kWh/kg efficiency of electrolyser (where 1kg = mass of H2 + O2)
+#   1 kWh = 3600000 J, so 18000000 J/kg efficient
+#   Energy = mass_kg * 18000000 J/kg
+#   Power = mass_kg * 18000000 J/kg/s
+# Need to turn propellant_used_kg_s of ice into H2 and O2 every second by melting ice and electrolysing.
+# powerFuelCreation = mass_kg * 146000 J/kg/s + mass_kg * 333550 J/kg/s + mass_kg * 18000000 J/kg/s)
+#   146000 + 333550 + 18000000 = 18479550  # so... melting ice negligable, electrolysis is everything
+# powerFuelCreation = mass_kg * 18479550
+def powerFuelCreation(engine_specs, engine_count)
+  # may need engine_specs[:propellant_used_kg_s]
+  return engine_specs[:propellant_used_kg_s] * 18479550.0
+end
+
+# deltaV_km_s = isp * g * ln ( (mass_asteroid_start_kg)) / mass_asteroid_end_kg)
+# (mass_asteroid_start_kg) / mass_asteroid_end_kg = e ^ (deltaV_km_s / (isp * g)
+# mass_asteroid_end_kg = mass_asteroid_start_kg / (e ^ (deltaV_km_s / (isp * g)))
+def massAsteroidEnd(deltaV_km_s, engine_specs, mass_asteroid_start_kg)
+  return mass_asteroid_start_kg / (Math::E ** (deltaV_km_s / (engine_specs[:isp_s] * 9.8)))
+end
+
+def timeOfJourney(p1='FIXME',p2='FIXME',p3='FIXME')
+  return -1
 end
 
 
 
-# Things we know: deltaV_km_s, mass_asteroid_kg, engine_type, engine_count
+# Things we know: deltaV_km_s, mass_asteroid_start_kg, engine_specs, engine_count
 # Things we want to know: time to achieve deltaV_km_s, end mass of asteroid
 
 # Givens
-deltaV_km_s = 1000
-mass_asteroid_start_kg = 2000000
+@deltaV_km_s = 1000
+@mass_asteroid_start_kg = 2000000
+@engine_count = 4
 
-puts "massAsteroidEnd for deltaV #{deltaV_km_s} asteroid mass #{mass_asteroid_kg} is #{massAsteroidEnd(deltaV_km_s, @ionEngine[:Isp_s], mass_asteroid_kg)}"
-
-puts "timeOfJourney for deltaV #{deltaV_km_s} asteroid mass #{mass_asteroid_kg} is #{massAsteroidEnd(deltaV_km_s, @ionEngine[:Isp_s], mass_asteroid_kg)}"
+# Now calculate for each type
+@engine_type.each do |engine_name, engine_specs|
+  puts "For deltaV #{@deltaV_km_s} asteroid mass #{@mass_asteroid_start_kg} and engine name #{engine_name}..."
+  puts "  massSolarArray  is  #{(massSolarArray(engine_specs, @engine_count)).round(3).to_s.rjust(12)} kg"
+  puts "  areaSolarArray  is  #{(areaSolarArray(engine_specs, @engine_count)).round(3).to_s.rjust(12)} m^2"
+  puts "  massSpacecraft  is  #{(massSpacecraft(engine_specs, @engine_count)).round(3).to_s.rjust(12)} kg"
+  puts "  massAsteroidEnd is  #{(massAsteroidEnd(@deltaV_km_s, engine_specs, @mass_asteroid_start_kg)).round(3).to_s.rjust(12)} kg"
+  puts "  massAsteroidUsed is #{(@mass_asteroid_start_kg - massAsteroidEnd(@deltaV_km_s, engine_specs, @mass_asteroid_start_kg)).round(3).to_s.rjust(12)} kg"
+  puts "  timeOfJourney  is   #{(timeOfJourney(@deltaV_km_s, engine_specs, @mass_asteroid_start_kg)).round(3).to_s.rjust(12)} days"
+  puts
+end
